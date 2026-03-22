cd4ad2aa99a8b28958a99fd0e078d436da710093
hariganesh-git <hariganesh.bikkina@gmail.com>
2026-03-22 15:53:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import urllib.parse
 import os
 import gdown
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('cosine_sim.pkl', 'rb'))
 
 url = "https://drive.google.com/uc?id=1iAMTPxBZ9aQuzXseuD0k2P4ymiq8V-LM"
-output = "model.pkl"
+output = "cosine_sim.pkl"
 
 if not os.path.exists(output):
     gdown.download(url, output, quiet=False)
